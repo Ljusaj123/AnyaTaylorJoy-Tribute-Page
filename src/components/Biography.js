2bcd9fd6d2b7631860e48fced62e3e9953ca018b
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-17 16:52:31 +0200

    jsx for Biography

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -1,7 +1,47 @@
 import React from "react";
 
 function Biography() {
-  return <div>Biography</div>;
+  return (
+    <main>
+      <div className="biography-container">
+        <div className="title-container">
+          <h1>Biography</h1>
+        </div>
+        <div className="biography-text-container">
+          <p>
+            Anya-Josephine Marie Taylor-Joy is an actress and model. She is the
+            recipient of several accolades, including a Golden Globe Award, a
+            Screen Actors Guild Award, and a Critics&#39; Choice Television
+            Award, in addition to nominations for a British Academy Film Award
+            and a Primetime Emmy Award. In 2021, she was featured inTime
+            magazine&#39;s &#39;Time 100 Next&#39; list.
+          </p>
+          <p>
+            Taylor-Joy received praise for portraying the lead characters Emma
+            Woodhouse in the Jane Austen adaptation Emma and Beth Harmon in the
+            Netflix miniseries The Queen&#39;s Gambit (both 2020); for the
+            latter, she won the Critics&#39; Choice Television Award for Best
+            Actress in a Movie/Miniseries, the Golden Globe for Best Actress
+            Miniseries or Television Film, the Screen Actors Guild Award for
+            Outstanding Performance by a Female Actor in a Miniseries or
+            Television Movie and received a nomination for the Primetime Emmy
+            Award for Outstanding Lead Actress in a Limited or Anthology Series
+            or Movie.
+          </p>
+          <p>
+            Remarking upon her performance in Thoroughbreds, David Ehrlich of
+            IndieWire called Taylor-Joy "one of the world's best young actors".
+            Her eyes, hair, cupid's bow, and accent have been highlighted as her
+            trademarks.Since 2020, she has been chosen as the ambassador for
+            brands such as Viktor Rolf and Tiffany Co.Since 2021, she has been a
+            global ambassador for Dior's fashion and makeup.As ambassador for
+            the brand, she is frequently seen wearing red Dior lipstick to award
+            ceremonies as her "signature" look.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Biography;
