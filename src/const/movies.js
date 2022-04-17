4d080017dd7add77b35450479c28d690f802d6fc
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-17 16:19:45 +0200

    added movies list

---
+++ src/const/movies.js
@@ -0,0 +1,70 @@
+const movies = [
+  {
+    picture_url: "/radioactive.jpeg",
+    title: "Radioactive",
+    year: "2019",
+    rating: 63,
+  },
+  {
+    picture_url: "/last-night-in-soho.webp",
+    title: "Last Night In Soho",
+    year: "2021",
+    rating: 75,
+  },
+  {
+    picture_url: "/The-Miniaturist.png",
+    title: "The Miniaturist ",
+    year: "2017",
+    rating: 76,
+  },
+  {
+    picture_url: "/Split-1.jpg",
+    title: "Split",
+    year: "2016",
+    rating: 77,
+  },
+  {
+    picture_url: "/Barry-Anya-Taylor-Joy.jpg",
+    title: "Barry",
+    year: "2016",
+    rating: 80,
+  },
+  {
+    picture_url: "/Emma.png",
+    title: "Emma",
+    year: "2020",
+    rating: 87,
+  },
+  {
+    picture_url: "/Thoroughbreds.png",
+    title: "Thoroughbreds",
+    year: "2017",
+    rating: 87,
+  },
+  {
+    picture_url: "/dar-crystal.jpg",
+    title: "The Dark Crystal: Age Of Resistance ",
+    year: "2019",
+    rating: 88,
+  },
+  {
+    picture_url: "/The-Witch.jpg",
+    title: "The Witch",
+    year: "2015",
+    rating: 90,
+  },
+  {
+    picture_url: "/Peaky-Blinders.png",
+    title: "Peaky Blinders",
+    year: "2013",
+    rating: 92,
+  },
+  {
+    picture_url: "/Queens-Gambit.png",
+    title: "The Queen's Gambit",
+    year: "2020",
+    rating: 96,
+  },
+];
+
+export default movies;
