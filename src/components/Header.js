6ebe66f714864fc714867c82c300998c946ca6d2
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-17 16:35:43 +0200

    new folder for components, component header created

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="header-container">
+        <h1 className="title">Anya Taylor-Joy</h1>
+        <blockquote>
+          “It is an entire world of just 64 squares. I feel safe in it. I can
+          control it; I can dominate it. And it is predictable. So, if I get
+          hurt, I only have myself to blame.”
+        </blockquote>
+        <p>From Queen&#39;s Gambit</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
