36fee9bfbd686e1185f56e555b44440874455e05
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-17 17:10:42 +0200

    included components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Biography from "./components/Biography";
+import Career from "./components/Career";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="home">
       <Header />
+      <Biography />
+      <Career />
     </div>
   );
 }
